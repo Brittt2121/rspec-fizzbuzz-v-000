cd56d679df2325bc14293606e704b0383395965f
Brittany Bellantoni <brittt2121@hotmail.com>
2018-01-30 23:14:00 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,11 @@
 def fizzbuzz(int)
-  if int % 5 == 0
-      "Buzz"
+  if int % 3 == 0
+      "Fizz"
     end
-  end
+    if int % 5 == 0
+        "Buzz"
+      end
+      if int % 5 == 0 && int % 3 == 0
+          "Fizzbuzz"
+        end
+      end
