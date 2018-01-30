213fec647b3cc1a3c07b95832da8a54af25792b3
Brittany Bellantoni <brittt2121@hotmail.com>
2018-01-30 23:25:13 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,6 @@
-def fizzBuzz(number)
-  if number % 3 == 0 && number % 5 == 0
-    "FizzBuzz"
-  elsif number % 3 == 0
+
+def fizzbuzz(int)
+  if int % 3 == 0 
     "Fizz"
-  elsif number % 5 == 0
-    "Buzz"
-  else
-    number
   end
 end
